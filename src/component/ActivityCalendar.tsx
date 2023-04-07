8a65abdc1ce9dc518fb92ab6e9734137b757657e
Jonathan Gruber <gruberjonathan@mail.com>
2023-04-07 21:58:19 +0200

    Update docs

--- src/component/ActivityCalendar.tsx
+++ src/component/ActivityCalendar.tsx
@@ -108,12 +108,12 @@ export interface Props {
    * Set the calendar colors for the `light` and `dark` system color scheme. The
    * color scale for at least one color scheme needs to be specified. For
    * undefined values, the default theme is used. By default, the calendar will
-   * use the currently set system color scheme. You can enforce a specific
+   * use the currently set system color scheme, but you can enforce a specific
    * color scheme with the `colorScheme` prop.
    *
    * Define each color scale explicitly with five colors or pass exactly two
    * colors (lowest and highest intensity) to calculate a single-hue scale.
-   * You can specify colors in any valid CSS format.
+   * Colors can be specified in any valid CSS format.
    *
    * Example:
    *
