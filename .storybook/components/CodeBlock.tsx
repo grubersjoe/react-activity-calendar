73fcabb0dbd1de574c94abcdbeb7bc281d17cc35
Jonathan Gruber <gruberjonathan@gmail.com>
2023-04-08 12:20:16 +0200

    Use light and dark syntax highlighting themes

--- .storybook/components/CodeBlock.tsx
+++ .storybook/components/CodeBlock.tsx
@@ -1,21 +1,30 @@
 import { CSSProperties, FunctionComponent } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { oneDark, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { useDarkMode } from 'storybook-dark-mode';
 
 type Props = {
   children: string;
   style?: CSSProperties;
 };
 
-const CodeBlock: FunctionComponent<Props> = ({ children }) => (
-  <SyntaxHighlighter
-    language="typescript"
-    customStyle={{ margin: '2rem 0', borderRadius: 3 }}
-    codeTagProps={{ className: 'syntax-highlighter' }}
-    style={tomorrow}
-  >
-    {children}
-  </SyntaxHighlighter>
-);
+const CodeBlock: FunctionComponent<Props> = ({ children }) => {
+  const isDark = useDarkMode();
+
+  return (
+    <SyntaxHighlighter
+      language="typescript"
+      customStyle={{
+        margin: '2rem 0',
+        borderRadius: 3,
+        ...(isDark && { background: '#222425' }),
+      }}
+      codeTagProps={{ className: 'syntax-highlighter' }}
+      style={isDark ? oneDark : oneLight}
+    >
+      {children}
+    </SyntaxHighlighter>
+  );
+};
 
 export default CodeBlock;
