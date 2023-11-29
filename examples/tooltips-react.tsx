77174d28e6631f1782725c80d9f2b94c367ff512
Jonathan Gruber <jonathan.gruber@gridfuse.com>
2023-11-29 13:05:53 +0100

    Upgrades

--- examples/tooltips-react.tsx
+++ examples/tooltips-react.tsx
@@ -1,15 +1,17 @@
 // @ts-nocheck
-import { Tooltip as ReactTooltip } from 'react-tooltip';
-import 'react-tooltip/dist/react-tooltip.css';
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 
-<ActivityCalendar
-  data={data}
-  renderBlock={(block, activity) =>
-    React.cloneElement(block, {
-      'data-tooltip-id': 'react-tooltip',
-      'data-tooltip-html': `${activity.count} activities on ${activity.date}`,
-    })
-  }
-/>
+<>
+  <ActivityCalendar
+    data={data}
+    renderBlock={(block, activity) =>
+      React.cloneElement(block, {
+        'data-tooltip-id': 'react-tooltip',
+        'data-tooltip-html': `${activity.count} activities on ${activity.date}`,
+      })
+    }
+  />
 
-<ReactTooltip id="react-tooltip" />
+  <ReactTooltip id="react-tooltip" />
+</>
