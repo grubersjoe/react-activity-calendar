19a56c2859be5dd9d696539d142410ee4d77f483
Jonathan Gruber <jonathan.gruber@gridfuse.com>
2024-06-20 00:27:33 +0200

    Upgrade to ESLint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+// @ts-check
+import { fixupPluginRules } from '@eslint/compat';
+import eslint from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import react from 'eslint-plugin-react/configs/recommended.js';
+import globals from 'globals';
+import typescript from 'typescript-eslint';
+
+export default typescript.config(
+  eslint.configs.recommended,
+  ...typescript.configs.strictTypeChecked,
+  {
+    ignores: ['build/', 'docs/', 'examples/', 'eslint.config.mjs', 'rollup.config.mjs'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/restrict-template-expressions': 'off',
+    },
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ...react,
+    languageOptions: {
+      ...react.languageOptions,
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    plugins: {
+      'react-hooks': fixupPluginRules(reactHooks),
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+);
