54befaedea72a535a1b0f8f110ec02bdfe0b68ee
Jonathan Gruber <jonathan.gruber@gridfuse.com>
2024-06-23 16:15:12 +0200

    Fix dark mode switch on docs page

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,26 +1,52 @@
-import { DocsContainer } from '@storybook/addon-docs';
-import type { DocsContainerProps } from '@storybook/blocks';
+import { DocsContainer, type DocsContainerProps } from '@storybook/blocks';
 import type { Preview } from '@storybook/react';
 import { type ThemeVarsPartial, themes } from '@storybook/theming';
-import { useDarkMode } from 'storybook-dark-mode';
+import { useEffect, useState } from 'react';
 
 import './storybook.scss';
 
-const common: ThemeVarsPartial = {
+const baseTheme: ThemeVarsPartial = {
   base: 'light',
   brandTitle: 'React Activity Calendar',
   brandUrl: 'https://github.com/grubersjoe/react-activity-calendar',
 };
 
-const Container = (props: DocsContainerProps) => (
-  <DocsContainer
-    {...props}
-    theme={{
-      ...common,
-      ...(useDarkMode() ? themes.dark : themes.light),
-    }}
-  />
-);
+const Container = (props: DocsContainerProps) => {
+  // We can't simply use `useDarkMode()` because Storybook sucks:
+  // "Storybook preview hooks can only be called inside decorators and story functions."
+  const [theme, setTheme] = useState(
+    document.body.classList.contains('dark') ? themes.dark : themes.light,
+  );
+
+  useEffect(() => {
+    const observer = new MutationObserver(mutations => {
+      for (const mutation of mutations) {
+        if (mutation.attributeName === 'class') {
+          setTheme(document.body.classList.contains('dark') ? themes.dark : themes.light);
+        }
+      }
+    });
+
+    observer.observe(document.body, {
+      attributes: true,
+      attributeFilter: ['class'],
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <DocsContainer
+      {...props}
+      theme={{
+        ...baseTheme,
+        ...theme,
+      }}
+    />
+  );
+};
 
 export const preview: Preview = {
   parameters: {
@@ -33,8 +59,8 @@ export const preview: Preview = {
     },
     darkMode: {
       stylePreview: true,
-      dark: { ...common, ...themes.dark },
-      light: { ...common, ...themes.light },
+      dark: { ...baseTheme, ...themes.dark },
+      light: { ...baseTheme, ...themes.light },
     },
   },
 };
