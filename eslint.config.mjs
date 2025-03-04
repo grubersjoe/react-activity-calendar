ec036b2d28e84f2c1ba490168fa3a5fca93b0726
Jonathan Gruber <gruberjonathan@gmail.com>
2025-03-04 10:25:52 +0100

    Upgrades

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */
 import eslint from '@eslint/js'
 import pluginReact from 'eslint-plugin-react'
 import pluginReactHooks from 'eslint-plugin-react-hooks'
